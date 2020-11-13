18f91f78ed8a31d9fe8d9fae80251945ac174a2c
makewheels <makewheels>
2020-11-13 21:59:57 +0800

    局域网真难

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
-    public const int maxHealth = 100;
+    public int maxHealth = 100;
+    [SyncVar(hook = "onChangeHealth")] public int currentHealth = 100;
+    public Slider healthSlider;
 
-    public int currentHealth = maxHealth;
 
     public void takeDamage(int damage)
     {
+        //血量处理只在服务器执行
+        if (isServer == false)
+        {
+            return;
+        }
+
         currentHealth -= damage;
         if (currentHealth <= 0)
         {
             currentHealth = 0;
+            RpcRespawn();
         }
+    }
+
+    private void onChangeHealth(int health)
+    {
+        //更新血条
+        healthSlider.value = health * 1.0f / maxHealth;
+    }
+
+    [ClientRpc]
+    private void RpcRespawn()
+    {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+
+        transform.position = new Vector3(0, 1.017199f, 0);
+        currentHealth = maxHealth;
     }
 
     // Use this for initialization
@@ -25,5 +53,6 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        healthSlider.transform.LookAt(Camera.main.transform);
     }
 }
